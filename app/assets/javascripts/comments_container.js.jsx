ddfcf4cecbef8bf4513e93632bb80538199f213d
veera <veerasekar.v.g@gmail.com>
2015-11-20 23:58:57 +1100

    adding fetch comments

--- app/assets/javascripts/comments_container.js.jsx
+++ app/assets/javascripts/comments_container.js.jsx
@@ -1,7 +1,13 @@
 var CommentContainer = React.createClass({
+  fetchComments() {
+    $.getJSON(
+      this.props.commentsPath,
+      (data) => this.setState({comments: data});
+    );
+  },
   getInitialState() {
     return { comments: [] };
-  }
+  },
   render() {
     return <Comment comments={this.state.comments} />;
   }
